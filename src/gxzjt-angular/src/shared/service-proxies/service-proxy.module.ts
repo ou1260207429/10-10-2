3c4979ad45d620e03b4377c715bc4819123a0721
wumin <654504857@qq.com>
2019-06-10 14:19:38 +0800

    修改前端配置

--- src/gxzjt-angular/src/shared/service-proxies/service-proxy.module.ts
+++ src/gxzjt-angular/src/shared/service-proxies/service-proxy.module.ts
@@ -14,8 +14,7 @@ import * as ApiServiceProxies from '@shared/service-proxies/service-proxies';
     ApiServiceProxies.AccountServiceProxy,
     ApiServiceProxies.ConfigurationServiceProxy,
     ApiServiceProxies.TenantRegistrationServiceProxy,
-
     { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true },
   ],
 })
-export class ServiceProxyModule {}
+export class ServiceProxyModule { }
