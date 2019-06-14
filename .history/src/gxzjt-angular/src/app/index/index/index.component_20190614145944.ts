da649c15c325f82fe82a6a6c9f1ea3dc4c082f2b
xhm <1250084414@qq.com>
2019-06-14 17:35:24 +0800

    添加办事指南详情页面

---
+++ .history/src/gxzjt-angular/src/app/index/index/index.component_20190614145944.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { HomeServiceProxy, PageSize } from '@shared/service-proxies/service-proxies';
+
+@Component({
+  selector: 'app-home-index',
+  styleUrls: ['./index.component.less'],
+  templateUrl: './index.component.html',
+})
+export class IndexComponent implements OnInit {
+  pageSize: PageSize = new PageSize();
+  constructor(private _homeServiceProxy: HomeServiceProxy) { }
+  currentIndex = 0;
+  downLoadList: any
+  lawsList: any
+  handleList:any;
+  ngOnInit(): void {
+    this.pageSize.page = 1;
+    this.pageSize.size = 10;
+    /**
+     * 
+     *查询表格列表
+     *  */
+    this._homeServiceProxy.homeTableDownloadList(this.pageSize).subscribe(data => {
+      this.downLoadList = data.data;
+    })
+    /**
+     * 查询法律法规
+     */
+    let params = this.pageSize
+    params.group = "Regulation";
+    this._homeServiceProxy.homeRegulationList(params).subscribe(data => {
+      this.lawsList = data.data;
+    })
+    /**
+     * 查询办事指南列表
+     */
+    this._homeServiceProxy.homeNoticeList(this.pageSize).subscribe(data => {
+      this.handleList = data.data;
+    })
+  }
+}
