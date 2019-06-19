861cbdf1d4a19e570bd95c24ff19d416ec82fba5
xuchang <3437983924@qq.com>
2019-06-19 23:57:07 +0800

    fix bug

---
+++ src/gxzjt-angular/src/app/pages/home/welcome/welcome.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+
+import { _HttpClient } from '@delon/theme';
+
+
+
+
+/**
+ * 抽查比例
+ */
+@Component({
+  selector: 'spot-welcome',
+  templateUrl: './welcome.html',
+  styles: [],
+})
+export class WelcomeComponent implements OnInit {
+
+  constructor(
+
+  ) {
+
+  }
+
+  ngOnInit() {
+ 
+  }
+
+
+
+
+}
