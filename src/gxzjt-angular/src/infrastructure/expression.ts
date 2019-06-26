84f364da5979d8154d3eacda0c1fe3e908fbd135
xuchang <3437983924@qq.com>
2019-06-26 19:40:01 +0800

    fix add

--- src/gxzjt-angular/src/infrastructure/expression.ts
+++ src/gxzjt-angular/src/infrastructure/expression.ts
@@ -3,7 +3,7 @@
 import { STPage, STComponent } from "@delon/abc";
 
 
-export const IS_DEBUG=true;
+export const IS_DEBUG=false;
 
 export const FLOW_SERVICES_URL =IS_DEBUG?'http://222.84.250.158:8111':'http://http://dn5.gxcic.net:8303';
 
