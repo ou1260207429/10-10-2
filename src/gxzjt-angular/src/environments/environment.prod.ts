6ab9de497442dbc574f1f5c856dbcfce702a34bb
xuchang <3437983924@qq.com>
2019-06-30 14:04:18 +0800

    md url

--- src/gxzjt-angular/src/environments/environment.prod.ts
+++ src/gxzjt-angular/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 
-export const IS_DEBUG = true;
+export const IS_DEBUG = false;
 export const environment = {
-  SERVER_URL: IS_DEBUG ? 'http://222.84.250.158:8111/' : `http://dn5.gxcic.net:8301/`,
+  SERVER_URL:'http://222.84.250.158:8111/',//: `http://dn5.gxcic.net:8301/`,
   production: true,
   useHash: true,
   hmr: false,
