80dd0a6487789227a785df140fb4648f17c239a3
huziyuan <472860318@qq.com>
2019-07-19 10:43:26 +0800

    注释撤回申请

--- src/gxzjt-angular/src/app/pages/work-matters/all-done/all-done.component.ts
+++ src/gxzjt-angular/src/app/pages/work-matters/all-done/all-done.component.ts
@@ -3,7 +3,8 @@ import { _HttpClient, ModalHelper } from '@delon/theme';
 import { STColumn, STComponent } from '@delon/abc';
 import { SFSchema } from '@delon/form';
 import { FormBuilder, FormGroup } from '@angular/forms';
-
+import { WorkMattersService } from '../../work-matters/work-matters.service';
+import { timeTrans } from 'infrastructure/regular-expression';
 @Component({
   selector: 'app-work-matters-all-done',
   templateUrl: './all-done.component.html',
@@ -12,7 +13,19 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class WorkMattersAllDoneComponent implements OnInit {
   rangeTime;
   fliterForm: FormGroup;
-
+  cityarray =[] ;
+  selectedcity;//存市
+  countyarray;//存县数组
+  param={
+    cityName:'',
+    area:'',
+    startApplyTime:'',
+    endApplyTime:'',
+    proName:'',
+    proType:'',
+    timetype:'',
+    buildname:'',
+  }
   @ViewChild('st') st: STComponent;
   columns: STColumn[] = [
     {
@@ -60,7 +73,10 @@ export class WorkMattersAllDoneComponent implements OnInit {
   constructor(private http: _HttpClient,
      private modal: ModalHelper,
      private formBuilder: FormBuilder,
-     ) { }
+     private WorkMattersService:WorkMattersService,
+     ) {
+       this.getCityList();
+      }
 
   ngOnInit() {
     this.fliterForm = this.formBuilder.group({
@@ -69,6 +85,8 @@ export class WorkMattersAllDoneComponent implements OnInit {
       proNo: [null],
       proName: [null],
       proType: [null],
+      buildname:[null],
+      timetype:[null],
       dateRange: [this.rangeTime],
 
     });
@@ -79,10 +97,91 @@ export class WorkMattersAllDoneComponent implements OnInit {
     //   .createStatic(FormEditComponent, { i: { id: 0 } })
     //   .subscribe(() => this.st.reload());
   }
+  resetForm(): void {
+    this.fliterForm = this.formBuilder.group({
+      city: [null],
+      count: [null],
+      proNo: [null],
+      proName: [null],
+      proType: [null],
+      buildname:[null],
+      timetype:[null],
+      dateRange: [this.rangeTime],
+
+    });
+    // this.getList();
+  }
+  getCityList() {
+    this.WorkMattersService.GetAreaDropdown().subscribe(
+      res => {
+
+        this.cityarray=JSON.parse(res.result).Children
+      },
+    );
+
+  }
+
+  search() {
+    if(this.fliterForm.controls.city.value){
+      this.param.cityName = this.fliterForm.controls.city.value;
+    }
+    if(this.fliterForm.controls.count.value){
+      this.param.area = this.fliterForm.controls.count.value;
+    }
+    if(this.fliterForm.controls.proName.value){
+      this.param.proName=this.fliterForm.controls.proName.value;
+    }
+    if(this.fliterForm.controls.proType.value){
+      this.param.proType=this.fliterForm.controls.proType.value;
+    }
+
+    if(this.fliterForm.controls.buildname.value){
+      this.param.buildname=this.fliterForm.controls.buildname.value;
+    }
+    if(this.fliterForm.controls.timetype.value){
+      this.param.timetype=this.fliterForm.controls.timetype.value;
+    }
+    // this.param.flowPathType = Number(this.fliterForm.controls.proType.value);
+    //   if (this.param.flowPathType == 0) {
+    //     this.param.flowPathType = -1;
+    //   }
+
+    if(this.fliterForm.controls.dateRange.value.length!=0){
+
+      this.param.startApplyTime=timeTrans(Date.parse(this.fliterForm.controls.dateRange.value[0]) / 1000, 'yyyy-MM-dd', '-')+" 00:00:00";
+      this.param.endApplyTime =timeTrans(Date.parse(this.fliterForm.controls.dateRange.value[1]) / 1000, 'yyyy-MM-dd', '-')+" 23:59:59";
+    }else{
+      this.param.startApplyTime='';
+      this.param.endApplyTime='';
+    }
+console.log(this.param)
+  }
+
   resetTime() {
     var startTime = new Date();
     startTime.setDate(startTime.getDate() - 30)
     this.rangeTime = [startTime, new Date()];
+  }
+  cityChange(e) {
+    this.fliterForm = this.formBuilder.group({
+      city: [e],
+      count: [null],
+      proNo: this.fliterForm.controls.proNo.value,
+      proName: this.fliterForm.controls.proName.value,
+      proType:this.fliterForm.controls.proType.value,
+      dateRange: this.fliterForm.controls.dateRange.value,
+      timetype:this.fliterForm.controls.timetype.value,
+      buildname:this.fliterForm.controls.buildname.value,
+
+    });
+    this.countyarray = []
+    this.cityarray.forEach(element => {
+      if (element.Name == e) {
+        this.countyarray = element.Children
+      }
+
+    });
+
   }
 
 }
