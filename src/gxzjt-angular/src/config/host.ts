b8b7908ac458e68ef8a9c438ff7dfa62c7dedce1
xhm <1250084414@qq.com>
2019-08-02 16:25:25 +0800

    单位项目办理统计新增导出功能

--- src/gxzjt-angular/src/config/host.ts
+++ src/gxzjt-angular/src/config/host.ts
@@ -9,7 +9,7 @@ export class URLConfig {
 
     private static instance = new URLConfig();
 
-    public SERVER_URL = "http://192.168.10.48:21022/";
+    public SERVER_URL = "http://222.84.250.158:8111/";
     public REGISTER_URL = "http://222.84.250.158:8000/";
     public XIEFENG_SERVICES_URL = 'http://222.84.250.158:8113/';
     public APP_URL = "./";
@@ -19,14 +19,14 @@ export class URLConfig {
     }
     // export var SERVER_URL =   'http://dn5.gxcic.net:8303';
 
-    // // export const SERVER_URL = IS_DEBUG ? 'http://192.168.10.48:21022' : 'http://192.168.10.48:21022';
+    // // export const SERVER_URL = IS_DEBUG ? 'http://222.84.250.158:8111' : 'http://222.84.250.158:8111';
     // // export const SERVER_URL = SERVER_URL;
 
     // //谢峰的服务地址
     // export var XIEFENG_SERVICES_URL = 'http://dn5.gxcic.net:8305';
 
     // //庞博的服务器上传图片地址
-    // // export var REGISTER_URL = "";//IS_DEBUG ? 'http://222.84.250.158:8000/' : 'http://192.168.10.48:21022/';
+    // // export var REGISTER_URL = "";//IS_DEBUG ? 'http://222.84.250.158:8000/' : 'http://222.84.250.158:8111/';
 
     // //注册地址，勿随意动
     // export var REGISTER_URL = "http://dn5.gxcic.net:8304";
