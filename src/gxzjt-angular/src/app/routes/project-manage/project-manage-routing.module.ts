cd905f7b8954c0cebd3f00173b32b69ac9ce7dbc
xuchang <3437983924@qq.com>
2019-08-05 19:32:19 +0800

    fix bug

--- src/gxzjt-angular/src/app/routes/project-manage/project-manage-routing.module.ts
+++ src/gxzjt-angular/src/app/routes/project-manage/project-manage-routing.module.ts
@@ -3,7 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProjectManageAddDesignComponent } from './add-design/add-design.component';
 
 const routes: Routes = [
-
+  {
+    path: '',
+    component: ProjectManageAddDesignComponent,
+    data: { title: '新增消防设计审查' }
+  },
   {
     path: 'add-design',
     component: ProjectManageAddDesignComponent,
